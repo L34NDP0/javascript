2c123907507981a56fd472adea447d5bfe67f721
L34NDP0 <105683560+L34NDP0@users.noreply.github.com>
2022-08-19 19:07:13 -0300

    Add files via upload

---
+++ condicoes.js
@@ -0,0 +1,15 @@
+//var vel = 60.5
+//console.log('A velocidade do carro é ' + vel + ' kmh!')
+//if (vel >60) {  //condição simples
+//    console.log('Sua velocidade foi maior que 60kmh, você foi multado')
+//}
+//console.log('Diriga com cuidado')
+
+var país = 'Brasil'
+console.log ('Vivendo em ' + país)
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}
+
